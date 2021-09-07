ec1da8a5e5e6b9547589ea595c0066d50d24bc18
Luis Vanegas <lvanegas1429@gmail.com>
2021-09-07 13:58:10 -0600

    Reactive Implementando

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,42 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Reactive-Forms';
+
+  dataForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.inializarFormulario();
+  }
+
+  createHijos(): FormGroup{
+    return this.fb.group({
+      nombre: ['', Validators.required],
+      apellido: ['', Validators.required],
+      edad: ['', Validators.required]
+    })
+  }
+
+  inializarFormulario(){
+    this.dataForm = this.fb.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required],
+      hijos: this.fb.array([this.createHijos()])
+    })
+  }
+
+  get hijos(){
+    return this.dataForm.get('hijos') as FormArray;
+  }
+
+  agregarHijos(){
+    this.hijos.push(this.createHijos());
+  }
+
+  borrarHijos(i: number){
+    this.hijos.removeAt(i);
+  }
+
+  guardar(){
+    console.log(this.dataForm.value);
+  }
 }
